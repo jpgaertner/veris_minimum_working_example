264a31d674dbd7a27aeaa3903847f0375c459a24
jpgaertner <jph.gaertner@gmail.com>
2025-07-18 17:07:58 +0200

    clarify variable name

--- parallel_hello_world.py
+++ parallel_hello_world.py
@@ -3,8 +3,8 @@ import sys
 
 size = MPI.COMM_WORLD.Get_size()
 rank = MPI.COMM_WORLD.Get_rank()
-name = MPI.Get_processor_name()
+node_name = MPI.Get_processor_name()
 
 sys.stdout.write(
     "Hello, World! I am process %d of %d on %s.\n"
-    % (rank, size, name))
+    % (rank, size, node_name))
