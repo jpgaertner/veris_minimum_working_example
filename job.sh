64373fdb1f027df25b69c07d5612d2acdf4270a0
jpgaertner <jph.gaertner@gmail.com>
2025-07-29 12:09:16 +0200

    introduce naming convention

--- job.sh
+++ job.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -l
 #SBATCH -p xxx
 #SBATCH -A xxx
-#SBATCH --job-name=test
-#SBATCH -t 0:10:00
+#SBATCH --job-name=1280_64_100.txt
+#SBATCH -t 4:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks=4
-#SBATCH --output=output.txt
+#SBATCH --ntasks=64
+#SBATCH --output=results/output_1280_64_100.txt
+#SBATCH --exclusive
 
 
-srun -n 4 --mpi=pmi2 --cpu-bind=core python3 parallel_hello_world.py
+srun -n 64 --mpi=pmi2 --cpu-bind=core python3 run_files/run_parallel_1280_64.py
