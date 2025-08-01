ce2927c453d1b2c2ea611ba4ab991c278b9e2702
jpgaertner <jph.gaertner@gmail.com>
2025-08-01 12:14:51 +0200

    clean up + enable float64 for jax

--- job.sh
+++ job.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -l
 #SBATCH -p xxx
 #SBATCH -A xxx
-#SBATCH --job-name=1280_64_100.txt
+#SBATCH --job-name=1280_64_100
 #SBATCH -t 4:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=64
+
 #SBATCH --output=results/output_1280_64_100.txt
 #SBATCH --exclusive
 
+export JAX_ENABLE_X64=True
 
-srun -n 64 --mpi=pmi2 --cpu-bind=core python3 run_files/run_parallel_1280_64.py
+srun --mpi=pmi2 --cpu-bind=core python3 run_files/run_parallel_1024_128.py
