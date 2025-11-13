c1cccefafc4a1332d64d03a7cd431158ff2c21a4
jpgaertner <jph.gaertner@gmail.com>
2025-11-13 15:38:15 +0100

    activate conda env here

--- job.sh
+++ job.sh
@@ -1,14 +1,21 @@
 #!/bin/bash -l
 #SBATCH -p xxx
 #SBATCH -A xxx
-#SBATCH --job-name=1280_64_100
-#SBATCH -t 4:00:00
-#SBATCH --nodes=1
-#SBATCH --ntasks=64
+#SBATCH --job-name=256_2_10
+#SBATCH -t 0:05:00
+#SBATCH --nodes=2
+#SBATCH --tasks-per-node=1
+#SBATCH --gpus-per-task=1
+#SBATCH --output=results/results/output_256_002_10.txt
+##SBATCH --exclusive
+#SBATCH --mem=480G
 
-#SBATCH --output=results/output_1280_64_100.txt
-#SBATCH --exclusive
 
 export JAX_ENABLE_X64=True
 
-srun --mpi=pmi2 --cpu-bind=core python3 run_files/run_parallel_1024_128.py
+source ~/.bashrc
+conda activate veris
+# this is necessary for using multiple gpus
+module load openmpi/4.1.5-nvhpc-24.9
+
+mpirun python3 -u run_files/run_parallel_256_002.py
